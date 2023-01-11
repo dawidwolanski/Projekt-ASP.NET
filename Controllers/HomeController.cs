36ea595b374543015302fdffea4d08837b3b628f
Dawid <Dawid@DESKTOP-OEQU4CD>
2023-01-11 12:18:53 +0100

    Dodaj pliki projektów.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using bank2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace bank2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
